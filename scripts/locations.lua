ec8db3ed42fab77a4f6e278b3863c81468dc7626
Swaga1icous <15239100+Swaga1icous@users.noreply.github.com>
2025-03-09 15:15:57 -0700

    Adding new maps and fixing typos
    Added Cloister, Gallery of Mirrors, Gloamwood, Oasis, Plague Forest, and Snow City maps courtesy of Azuri Caelum, vgm and Black Soul Knight
    
    Fixed typo for forgotten City
    Changed Verdazure Sea (East) to Verdazure Ocean (East)

--- scripts/locations.lua
+++ scripts/locations.lua
@@ -1,4 +1,14 @@
 -- Ana Thema
 Tracker:AddLocations("locations/anaThema.jsonc")
+-- Gallery of Mirrors
+Tracker:AddLocations("locations/galleryMirrors.jsonc")
+-- Gallery of Mirrors
+Tracker:AddLocations("locations/cloister.jsonc")
+-- Gallery of Mirrors
+Tracker:AddLocations("locations/snowCity.jsonc")
+-- Gallery of Mirrors
+Tracker:AddLocations("locations/gloamwood.jsonc")
+-- Gallery of Mirrors
+Tracker:AddLocations("locations/plagueForest.jsonc")
 -- Transitions
 Tracker:AddLocations("locations/Transitions.jsonc")
